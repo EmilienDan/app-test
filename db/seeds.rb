1ae4c0679edbc9287d008a4abea84beee000581e
Emilien Danielou <danielou.emilien@gmail.com>
2025-04-28 15:44:00 +0200

    index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Booking.destroy_all
+puts "Booking destroyed"
+Offer.destroy_all
+puts "Offers destroyed"
+User.destroy_all
+puts "Users destroyed"
+
+user1 = User.create!(email: "mario@gmail.com", password: "abcdefg")
+user2 = User.create!(email: "luigi@gmail.com", password: "abcdefg")
+
+Offer.create!(user: user1, hotel_name: "Bling Bling Palace", description: "Super cool place!", location: "Nantes, France", price: 75, photo_url: "palace.jpg")
+Offer.create!(user: user2, hotel_name: "Hotel F1", description: "Shit hole", location: "Paris, France", price: 20, photo_url: "hotelf1.jpg")
